5a9afdd0f80fd1cbdc2797ff7d60ae165744bfac
03969402000 <03969402000@LAB205-30.fw.iffar.edu.br>
2022-10-06 20:49:15 -0500

    semana 2 - classes de conexão

---
+++ Estacionamento/src/connection/ConnectionFactory.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author 03969402000
+ */
+public class ConnectionFactory {
+
+    private static final String DRIVER = "com.msql.jdbc.Driver";
+    private static final String URL = "jbdc:mysql://localhost:3306/estacionamento";
+    private static final String USER = "root";
+    private static final String PASS = "";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException("Erro na conexão: ", e);
+        }
+
+    }
+
+    public static void closeConnection(Connection con) {
+        try {
+            if (con != null) {
+                con.close();
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao desconectar: ", e);
+        }
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement stmt) {
+
+        closeConnection(con);
+        try {
+            if (stmt != null) {
+                stmt.close();
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao desconectar: ", e);
+        }
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs) {
+
+        closeConnection(con, stmt);
+        try {
+            if (rs != null) {
+                rs.close();
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao desconectar: ", e);
+        }
+    }
+}
+

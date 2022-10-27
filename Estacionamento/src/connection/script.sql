bebff168a46387b67f6c795f9f9b33c91464297d
03969402000 <03969402000@LAB205-30.fw.iffar.edu.br>
2022-10-27 14:12:50 -0500

    semana 5

--- Estacionamento/src/connection/script.sql
+++ Estacionamento/src/connection/script.sql
@@ -17,3 +17,18 @@ numero int NOT NULL,
 rua varchar(100) NOT NULL,
 obliqua boolean NOT NULL,
 PRIMARY KEY (idVaga));
+
+
+CREATE TABLE motorista ( 
+idMotorista int NOT NULL AUTO_INCREMENT,
+nome varchar (100) NOT NULL,
+genero varchar (20) NOT NULL,
+RG int NOT NULL, 
+CPF int NOT NULL, 
+celular int NOT NULL,
+email varchar (50) NOT NULL,
+senha varchar (10) NOT NULL, 
+PRIMARY KEY (idMotorista));
+
+
+
